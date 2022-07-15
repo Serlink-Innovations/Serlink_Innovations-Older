fa8740717fa56cd4081a59743fb45ba23f087527
Serlink-Innovations <serlink.business@outlook.com>
2022-07-15 01:22:12 -0400

    v1.0.4

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
-const { normalize } = require("path");
+//Mobile Navigation Toggler
+const navToggler = document.querySelector('#navToggler');
+const nav = document.querySelector('#nav');
+
+navToggler.addEventListener('click', () => {
+    nav.classList.toggle('active');
+});
